9fa7036fc5143d8eba7ee6f85328c99232dffffc
vikram-bosak <vikramkumarbosak69@gmail.com>
2022-07-29 10:13:52 +0530

    this is a note during a live class

---
+++ book-and-pan.js
@@ -0,0 +1,38 @@
+// => Slack is a messaging program designed specifically for the office, but has also been adopted for personal use.
+
+// vaible name canot be a string
+
+let a = 45;  // js is case sensitive
+
+let na me = "vikra bosak" // while declearing variable i cant put a space in between variable name
+
+// why use camment => camment for for dev worker
+let = x = 10;
+
+//  variable name canot start with a Number.
+
+// let 123ab =18;
+
+// => 2 variable name cannot start with a symbol
+
+let @abe = 99;
+let #fsf = 345;
+
+// expect ~#
+
+// => 3 variable name should be self expplanatory
+
+let name "vikram";
+let father_name = "baikunth bosak"
+let mother_name = "sareta devi"
+
+// => 4 variable names cannot be keywprd
+
+// let , var const , for , while , new, this,
+
+// let for = 34;
+
+// => 5 let check case
+
+let first_name="vikram"  // this is snake case
+let firstName="bosak"  // this is camel case 
---
+++ problem4.js
@@ -0,0 +1,11 @@
+let name = "vikram kumar bosak";
+let school = "Masai School";
+let grade = "A1";
+let Section = "Web dev";
+let Rollno = "01";
+let DSA = "100";
+let JavaScript = "100";
+let CommnicationCheck = "100";
+
+
+console.log(name);
