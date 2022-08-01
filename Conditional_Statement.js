3c1ba931ac5b6b1e29bd466165e5debff7aaf73d
vikram-bosak <vikramkumarbosak69@gmail.com>
2022-08-01 11:57:31 +0530

    today is my 7th class of massai the class ended now

---
+++ Conditional_Statement.js
@@ -0,0 +1,84 @@
+
+// => |Today is a 7th day of my masai journey and we will learn today condition statement in javascript
+
+// -> Today's date is 1Aug 2022
+
+
+// let a = 10;
+// let b = 20;
+
+// // if (condition) {   // this is sytex
+
+// // }
+
+// if(b>a){
+//     console.log("b  is greater")
+// }
+
+// thwo numbers are given, print " tThere are equal" if ther two numberws are equal
+
+
+
+// let a = 17;
+
+// // let b = 17;
+
+// // if(a==b){
+// //     console.log("They are Equal")
+// // }
+
+
+
+// // if else - statement
+
+// let a = 17;
+
+// let b = 17;
+
+// if(a==b){
+//     console.log("They are Equal"){
+
+//     }else{
+//         console.log("There are not Equal");
+//     }
+
+// }
+
+
+// let total_bill=699;
+// let discount_starts=500;
+
+// // pribnt the new price agter a dicount of 10% if the custamer is eligible
+
+// if (total_bill>=discount_starts) {
+//     let new_bill=total_bill-(0.10)*total_bill
+//     console.log("new_bill")
+
+// } else {
+//     console.log("The custamer is notegible for discout")
+
+// }
+
+// let num=99;
+
+// if(num%10==0){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+
+
+//=> if elseif if statement
+// =>
+
+
+//=>  1000 dono
+
+
+let a = 10;
+let b = "10";
+
+// console.log(((a+b) - (b-a))+((a**b)-a-b));
+
+
+console.log(a===b);
