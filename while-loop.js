432b4f87771904120e0d97f89da672cfcb16cf25
vikram-bosak <vikramkumarbosak69@gmail.com>
2022-08-04 09:50:17 +0530

    today 8th day of masai school.

---
+++ while-loop.js
@@ -0,0 +1,145 @@
+// increment
+
+// x++;
+// ++x;
+
+// let x =10;
+
+// let y = x++;   // 1 assign the value of  x to y , increase the value of x
+
+
+// this is post increment
+
+
+// post increment
+
+// let x = 10;
+
+// let y = x++;
+
+// step1 : Assinging the value of x to y
+
+//  step2 : Incrementing the value of  x
+
+
+// console.log(x);
+
+// console.log(y);
+
+// pre increment
+
+// let x = 11;
+
+// let y = x++;  // post increment
+
+// let z = ++x;   // pre increment
+
+// step1 : Assinging the value of x to y
+
+//  step2 : Incrementing the value of  x
+
+
+// console.log(x);
+
+// console.log(y);
+
+// console.log(z);
+
+
+
+// post dec , pre decrement
+
+
+// let x = 10;
+
+// let y = --x;
+
+// let z = x--;
+
+// let a= 6;
+// let b= a--;
+// let c=10;
+// let d=--c;
+
+// console.log(a,b,c,d);
+
+
+
+//=> while loops
+
+// // syntax of while loop
+
+// // print the numbere from 15 to 1
+
+// let i =1;
+
+// while(i<=12){
+
+//     console.log(i);
+//    if(i%3==0){
+
+//    }else{
+//        i++;
+
+//    }
+
+
+// }
+
+
+// break means get out from the loop
+
+
+// let i =1;
+//  while(i<=10){
+//     i++;
+//     if(i==3 || i==9){
+//         continue;
+//     }
+
+//     console.log(i);
+//  }
+
+
+
+// if(true && true){
+//     if(false ||true){
+//         console.log("Hello!");
+//     }
+
+
+
+// }
+
+// let a = 12;
+// let b =11;
+
+
+// if(a**b>=1000){
+//     if (a==b || a>b){
+//         if(a!=b){
+//             a=a*b;
+//             b=b/b;
+//         }
+//     }
+// }
+
+
+if(true && true){
+    if(!false || !true){
+        if (a==b){
+            a=b;
+            b=a;
+            console.log(a);
+            console.log(b);
+        }
+    }
+}
+
+// console.log(a,b);
+
+
+//  today is my 9th day of masai today's topic for loop in javascript
+
+
+
