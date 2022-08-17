22f975a723a0b8afcb60032290f0952fd10947f7
Vikram Bosak <107077620+Vikram-Bosak@users.noreply.github.com>
2022-08-17 02:50:37 +0000

    this is my function 2 live class at masai

---
+++ Objects-II.js
@@ -0,0 +1,19 @@
+// ehat is the frequecy   if each element of array
+
+// let arr=[1,2,2,3,4,4,4,5];
+
+// let ob={};
+
+// for (let i=0; i<=arr.length-1; i++){
+//     if(obj[arr[i]]==undefined){
+//         obj[arr[i]]=1;
+//     }else{
+//         obj[arr[i]]++;
+//     }
+// }
+
+// console.log(obj);
+
+
+// calcuate the sum of Element , whose occurance is 2
+
