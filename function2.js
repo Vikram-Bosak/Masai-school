636244a63a8c7ee25b9fe4cc0fdd6bcd38e45530
Vikram Bosak <107077620+Vikram-Bosak@users.noreply.github.com>
2022-08-17 05:36:24 +0000

    Today is my 22th day of masai school

---
+++ function2.js
@@ -0,0 +1,138 @@
+// problem1
+//= the the function to check if the number is even or old
+
+function check_number(x){
+if(x%2==0){
+    console.log("yes")
+}else{
+    console.log("No");
+}
+}
+
+check_number(2,1);
+
+
+
+//1. ---------------------------------
+
+
+
+//=> write a function to check if the number is even or odd if the number is even then check is it a multiple of 4  or not
+
+function check_number(x){
+if(x%2==0){
+    return x; // returining enven number
+    // console.log("yes")
+}else{
+    // console.log("No");
+    return false;
+}
+}
+
+let num=check_number(8);  //calling statement
+
+// when i have to perform any added operation on the outcome of a function, i will use return
+
+
+if(num%4==0){
+    console.log(num, "is a multiple of 4");
+}else{
+     console.log(num, "is a not multiple of 4");
+}
+
+
+
+// 2. -----------------------------------------
+
+
+
+function check_number(x){
+    if(x%2==0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+// if a number is even the function will give true as tthe output m otherwise false
+
+let result= check_number(50);
+
+// console.log(result);    case 1
+
+
+if(result==true){
+    console.log("yes, it is even");
+}else{
+    console.log("Odd number");
+}
+
+
+
+// 3 ==> --------------------------------------------------
+
+// using the previously defined function check even and odd number 1 and 50;
+
+
+for(let i=1; i<=50; i++){
+    let res = check_number(i);
+    if(res==true){
+        console.log(i, " is even");
+    }else{
+        console.log(i,"is odd");
+    }
+}
+
+
+// 4.---------------------------------------
+
+// problem4
+
+// write a function to reverse a string
+
+
+let str=["amanl"]
+
+
+function reverse(str){
+let bag ="";
+
+for(let i=str.length-1; i>=0; i--){
+    bag =bag+str[i];
+
+}
+
+console.log(bag);
+}
+
+reverse("vikram");
+
+
+
+// 5.---------------------------------------
+
+// using the reverse Function , check if the string is a pallindrome or not naman
+
+
+let string= "naman";
+
+function reverse(str){
+let bag ="";
+
+for(let i=str.length-1; i>=0; i--){
+    bag =bag+str[i];
+
+}
+
+return bag;
+}
+
+let rev_str=reverse(string);
+
+// console.log(rev_str);
+
+if(string==rev_str){
+    console.log("pallindrome");
+}else{
+    console.log("Not a Pallindrome");
+}
