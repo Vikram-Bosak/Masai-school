878de4d06b5ece522fd2be0dcdc1110a1a3d0985
Vikram Bosak <107077620+Vikram-Bosak@users.noreply.github.com>
2022-08-19 08:37:34 +0000

    today is 26th day of masai school

---
+++ Brute-Force.js
@@ -0,0 +1,19 @@
+
+let N = 7;
+factors = 0;
+
+for (let i = 0; i <= N; i++) {
+    if (N % i == 0) {
+        factors++;
+    }
+}
+if(factors==2){
+    console.log("prime");
+}else{
+    console.log("Not prime");
+}
+
+
+str = "masai";
+
+b 
---
+++ Javascript-XVI-Multi-Dimensional-Arrays.js
@@ -0,0 +1,19 @@
+// Javascript - XVI Multi Dimensional Arrays
+
+
+// let arr = [1, 2, 3,[4, 5, 6,],[7, 8, 9]];
+
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+
+
+let arr = [
+    ["ajay", 46,"Goa"],
+    ["vikram","25","bihar"]
+];
+
+
+// console.log(arr[0][1]);
+// console.log(arr[0][2]);
+console.log(arr[1][1][1]);
